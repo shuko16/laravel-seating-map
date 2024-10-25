396a4c188879a9c30d28f84357a4bf4a10ed1dca
watanabeHi <hide3@mild.ocn.ne.jp>
2024-10-26 05:27:00 +0900

    Add validation and remove navigation

--- app/Http/Controllers/SeatController.php
+++ app/Http/Controllers/SeatController.php
@@ -5,17 +5,34 @@ namespace App\Http\Controllers;
 use App\Models\Seat;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class SeatController extends Controller
 {
+    public function index()
+    {
+        $seatedList = Seat::whereSeatType(1)->pluck('seat_no')->toArray();
+        $emptyList = Seat::whereSeatType(2)->pluck('seat_no')->toArray();
+        $seatUsersList = Seat::whereSeatType(1)->with('user')->get();
+        $seatUsers = [];
+        foreach ($seatUsersList as $seatUser) {
+            $seatUsers[$seatUser->seat_no] = $seatUser->user->name ?? 'なし';
+        }
+        return view('index', compact('seatedList', 'emptyList', 'seatUsersList', 'seatUsers'));
+    }
+
     public function offSeat($seat_no)
     {
-        // Seat::whereSeatNo($seat_no)
-        //     ->whereNot('user_id', Auth::user()->id)
-        //     ->get()
-        //     ->count()
-        //     >= 1
-            
+        if (
+            Seat::whereSeatNo($seat_no)
+            ->whereNot('user_id', Auth::user()->id)
+            ->get()
+            ->count()
+            >= 1
+        ) {
+            throw ValidationException::withMessages(['otherUserSeated' => 'other user already seated.']);
+        };
+
         $seat = Seat::whereSeatNo($seat_no)->get()->first();
         $seat->seat_type = 2;
         $seat->user_id = null;
@@ -37,9 +54,9 @@ class SeatController extends Controller
         $seat = Seat::whereSeatNo($seat_no)->get()->first();
         $seat->seat_type = 1;
         $seat->user_id = Auth::user()->id;
-       
+
         $seat->save();
-        
+
         return redirect()->route('index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,49 +2,20 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SeatController;
-use App\Models\Seat;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', function () {
-    return view('layout');
-})->name('index');
-
-Route::get('/dashboard', function (Request $request) {
-    if ($request->user_id) {
-        if (Seat::where('user_id', $request->user_id)->count()) {
-            $seated_data = Seat::where('user_id', $request->user_id)->first();
-            $seated_data->user_id = null;
-            $seated_data->save();
-        }
-
-        $seat = Seat::find($request->seat_id);
-        $seat->user_id = $request->user_id;
-        $seat->save();
-    }
-    $seats = Seat::all();
-    return view('dashboard', ['seats' => $seats]);
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::post('/delete/{user_id}', function ($user_id) {
-    $seat = Seat::where('user_id', $user_id)->first();
-    $seat->user_id = null;
-    $seat->save();
-    $seats = Seat::all();
-    return view('dashboard', ['seats' => $seats]);
-})->middleware(['auth', 'verified'])->name('delete');
-
-Route::post('/offSeat/{seat_no}', [SeatController::class, 'offSeat'])->name('offSeat');
-Route::post('/onSeat/{seat_no}', [SeatController::class, 'onSeat'])->name('onSeat');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/seat', [SeatController::class, 'index'])->name('index');
+    Route::post('/offSeat/{seat_no}', [SeatController::class, 'offSeat'])->name('offSeat');
+    Route::post('/onSeat/{seat_no}', [SeatController::class, 'onSeat'])->name('onSeat');
 });
 
 require __DIR__ . '/auth.php';

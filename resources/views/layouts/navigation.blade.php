77d23e80b9144d001d264102a29eb81bf72d8802
shuko16 <shuko16@gmail.com>
2024-11-09 15:02:58 +0900

    Web.php完了

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,9 +12,9 @@
 
                 <!-- Navigation Links -->
                 {{-- <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex"> --}}
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    {{-- <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
-                    </x-nav-link>
+                    </x-nav-link> --}}
                 {{-- </div> --}}
                 
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
@@ -74,8 +74,8 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+            <x-responsive-nav-link :href="route('index')" :active="request()->routeIs('index')">
+                {{ __('layout') }}
             </x-responsive-nav-link>
         </div>
 
--- routes/web.php
+++ routes/web.php
@@ -2,29 +2,11 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SeatController;
-use App\Models\Seat;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/dashboard', function (Request $request) {
-    if ($request->user_id) {
-        if (Seat::where('user_id', $request->user_id)->count()) {
-            $seated_data = Seat::where('user_id', $request->user_id)->first();
-            $seated_data->user_id = null;
-            $seated_data->save();
-        }
-
-        $seat = Seat::find($request->seat_id);
-        $seat->user_id = $request->user_id;
-        $seat->save();
-    }
-    $seats = Seat::all();
-    return view('dashboard', ['seats' => $seats]);
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 
 Route::middleware('auth')->group(function () {

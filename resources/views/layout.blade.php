e2c47282e71efb0fda28d278ebb6c9c671f0ae8d
shuko16 <shuko16@gmail.com>
2024-11-19 14:23:12 +0900

    Error revised

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,28 +9,24 @@
             {{ $errors->first('otherUserSeated') }}
         </div>
         <div class="max-w-full mx-auto sm:px-6 lg:px-8">
-
             <div class="bg-white shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-
                     <div class="grid grid-cols-2 text-center">
                         <div>
                             <div class="grid grid-cols-12 text-center">
                                 @for ($i = 1; $i < 421; $i++)
                                     @include('components.seat-layout')
                                 @endfor
-
                             </div>
-
+                        </div>
+                        <div>
                             <div class="grid grid-cols-12 text-center">
                                 @for ($i = 421; $i < 842; $i++)
                                     @include('components.seat-layout')
                                 @endfor
-
                             </div>
                         </div>
                     </div>
-
                 </div>
             </div>
         </div>
